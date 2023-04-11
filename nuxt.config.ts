3e0a056e0af8c08f1d01baf292a92ac0edce84df
ctang <celenatang@yahoo.com>
2023-04-11 18:20:56 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+        vite: {
+        server: {
+            fs: {
+                allow: ["/Users/ctang/dropbox/c/c projects/10 Sites/test-directory/test-app/*"]
+            }
+        }
+    }
 
 })
